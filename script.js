12077ab15daa95f3bfb0e44f3c6ea4bca454dfa6
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-07 00:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function playSound(soundName) {
+    var audio = new Audio("sounds/" + soundName);
+    audio.play();
+}
+
+function stopAllSounds() {
+    var sounds = document.querySelectorAll("audio");
+    sounds.forEach(function(sound) {
+        sound.pause();
+        sound.currentTime = 0;
+    });
+}
